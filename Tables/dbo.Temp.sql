7f736a009dbd599f425fd6f20db1913d92fb7136
manish kumar <manish.kumar@durlabhcomputers.com>
2013-06-21 19:17:20 +0530

    temp table change

--- Tables/dbo.Temp.sql
+++ Tables/dbo.Temp.sql
@@ -1,6 +1,7 @@
 CREATE TABLE [dbo].[Temp]
 (
 [TempId] [int] NOT NULL,
-[des] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[des] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[qwewerew] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
