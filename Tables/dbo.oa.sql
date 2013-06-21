eee9be581da9752ea5fb5ae65b0c4aa0edc50200
manish kumar <manish.kumar@durlabhcomputers.com>
2013-06-21 18:17:29 +0530

    common

---
+++ Tables/dbo.oa.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[oa]
+(
+[Pieces] [int] NULL,
+[Pallets] [int] NULL,
+[Volumes] [decimal] (38, 3) NULL,
+[Weight] [decimal] (38, 4) NULL,
+[TripOrderId] [int] NULL,
+[TripId] [int] NULL
+) ON [PRIMARY]
+GO
---
+++ Tables/dbo.t1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[t1]
+(
+[ActualOrderIdOfTasks] [int] NOT NULL,
+[MinSortOrder] [int] NULL,
+[TripId] [int] NOT NULL
+) ON [PRIMARY]
+GO
