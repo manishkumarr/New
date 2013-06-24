d3ada9f6c2745a3325aa0d49332ad560f50ba0b7
manish kumar <manish.kumar@durlabhcomputers.com>
2013-06-24 13:49:00 +0530

    Email column added

--- Tables/dbo.Temp.sql
+++ Tables/dbo.Temp.sql
@@ -2,6 +2,7 @@ CREATE TABLE [dbo].[Temp]
 (
 [TempId] [int] NOT NULL,
 [des] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[qwewerew] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[qwewerew] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Email] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
